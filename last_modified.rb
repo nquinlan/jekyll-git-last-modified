94672097f5911dbb2b2c3d69a2e57ba6ba5fb7bf
Nick Quinlan <nicholas.quinlan@sendgrid.com>
2014-06-05 16:38:40 -0700

    removed some whitespace

--- last_modified.rb
+++ last_modified.rb
@@ -17,6 +17,7 @@ module LastModified
 	def set_last_modified_date(post_or_page)
 		entity_source = source(post_or_page)
 		last_modified = `git log -1 --format="%ad" -- "#{entity_source}"`
+		last_modified.strip!
 		post_or_page.data["last_modified"] = last_modified
 	end
   end
